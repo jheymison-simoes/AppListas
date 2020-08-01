fb4aaa823a16ca651f986f9eb813b8e83959113a
Jheymison <jheymisonbao@live.com>
2020-07-31 23:24:32 -0300

    Alterações Terminado Lista de Conjuntos

--- assets/my-js/conjuntos.js
+++ assets/my-js/conjuntos.js
@@ -88,12 +88,7 @@ class Set {
         });
 
         return subconjunto;
-    }
-
-    print(){
-        let conjuntos = this.items;
-        exibeConjunto(Object.values(conjuntos));
-    }   
+    }    
 }
 
 // Notificações de erro, successo e alerta
@@ -108,80 +103,151 @@ let enter = document.addEventListener('keypress', function(e){
     }
  }, false);
 
-// Esta função exibe o conjunto
-function exibeConjunto(conjunto, name) {
-    name = document.getElementById("conjunto").value;
-    let label = document.createElement("div");
-    let textoLabel = document.createTextNode(name);
-    label.appendChild(textoLabel);
-    label.className = "btn btn-link";
-    label.setAttribute("name", name);
+//  imprime os valores de um conjunto
+function exibeValues(){
+    let conjuntoAtual = document.getElementById("apresentaConjunto").value;
+    let list = document.getElementById("resposta");
+    list.innerHTML = "";
 
-    let node = document.createElement("div");
-    let texto = document.createTextNode(conjunto);
-    node.appendChild(texto);
-    node.className = "btn btn-info";
-    node.setAttribute("id", name);
-    
-    let conjuntos = document.getElementById("apresentaConjunto");
-    conjuntos.appendChild(label, conjuntos.childNodes[0]);
-    conjuntos.appendChild(node, conjuntos.childNodes[0]);
+    Object.keys(nomeDoArray[conjuntoAtual].items).forEach((itemDoArray, index) => {
+        let node = document.createElement("div");
+        node.className = "btn btn-success";
+
+        let textnode = document.createTextNode(itemDoArray);
+        node.appendChild(textnode);
+        list.appendChild(node);
+    });
 }
 
-function newConjunto(nameConj){
-    nameConj = document.getElementById("conjunto").value;
-    let nomeConjunto = nameConj;
-    
-    if(nameConj == ""){
-        msg = "Preencha os Campos Conjunto e Elemento!";
+// Imprimie os conjuntos em um Select
+function exibeConjunto(nomeDoArray) {
+    let list = document.getElementById("apresentaConjunto");
+    list.innerHTML = "";
+    Object.keys(nomeDoArray).forEach((itemDoArray, index) => {
+        let node = document.createElement("option");
+
+        let textnode = document.createTextNode(itemDoArray);
+        node.appendChild(textnode);
+        list.appendChild(node);
+    });
+}
+
+// Cria um novo conjunto
+function newSet(){
+    let novoConjunto = new Set();
+    let nomeConjunto = document.getElementById("conjunto").value;
+
+    if(nomeConjunto == "" || nomeConjunto == null){
+        msg = "Preencha o Campo!";
         tipo = "alert-warning";
         document.getElementById("avisos").innerHTML = notification(msg, tipo);
         document.getElementById("conjunto").focus();
     } else {
-        nameConj = new Set();
+        document.getElementById("conjunto").value = "";
+        nomeDoArray[nomeConjunto] = novoConjunto;
+        exibeConjunto(nomeDoArray);
+        console.log(nomeDoArray);
         msg = "Conjunto <u>" + nomeConjunto + "</u> adicionado com sucesso!";
         tipo = "alert-success";
         document.getElementById("avisos").innerHTML = notification(msg, tipo);
-        console.log(nameConj);
-        document.getElementById("conjunto").value = "";
-        document.getElementById("addvalor").value = "";
-
-        return nameConj;
+        document.getElementById("apresentaConjunto").focus();
     }
 }
 
-// let result = newConjunto("conjunto1");
+// cria valor para determinado conjunto
+function newValue(){
+    let conjuntoAtual = document.getElementById('apresentaConjunto').value;
+    let elementoAtual = document.getElementById('addvalor').value;
 
-function newValue(conjunto, elemento){
-    conjunto = document.getElementById("conjunto").value;
-    elemento = document.getElementById("addvalor").value;
-
-    console.log(conjunto, elemento);
-
-    conjunto.add(elemento);
+    if(elementoAtual == "" || elementoAtual == null){
+        msg = "Preencha o Campo!";
+        tipo = "alert-warning";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+        document.getElementById("addvalor").focus();
+    } else {
+        nomeDoArray[conjuntoAtual].add(elementoAtual);
+        exibeValues();
+        document.getElementById("addvalor").value = "";
+        msg = "O valor <u>" + elementoAtual + "</u> foi adicionado com sucesso no conjunto <u>" + conjuntoAtual + "";
+        tipo = "alert-success";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+        document.getElementById("addvalor").focus();
+    }    
 }
 
-// function addValor(){
-//     let name = document.getElementById("conjunto").value;
-//     let elemento = document.getElementById("addvalor").value;
+function removeValue(){
+    let conjuntoAtual = document.getElementById('apresentaConjunto').value;
+    let elementoAtual = document.getElementById('addvalor').value;
 
-//     if(name == "" || elemento == ""){
-//         msg = "Preencha os Campos Conjunto e Elemento!";
-//         tipo = "alert-warning";
-//         document.getElementById("avisos").innerHTML = notification(msg, tipo);
-//         document.getElementById("conjunto").focus();
-//     } else {
-//         console.log(newValue(name, elemento));
-//         msg = "Valor <u>" + elemento + "</u> adicionado no conjunto <u>" + name +"</u> com sucesso!";
-//         tipo = "alert-success";
-//         document.getElementById("avisos").innerHTML = notification(msg, tipo);
+    if(elementoAtual == "" || elementoAtual == null){
+        msg = "Preencha o Campo!";
+        tipo = "alert-warning";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+        document.getElementById("addvalor").focus();
+    } else {
+        nomeDoArray[conjuntoAtual].remove(elementoAtual);
+        exibeValues();
+        document.getElementById('addvalor').value = "";
+        msg = "O valor <u>" + elementoAtual + "</u> foi removido do conjunto <u>" + conjuntoAtual + "</U> com sucesso!";
+        tipo = "alert-info";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+        document.getElementById("addvalor").focus();
+    }
+}
 
-//         document.getElementById("conjunto").value = "";
-//         document.getElementById("addvalor").value = "";
-//     }
-// }
+function emptyItems(){
+    let conjuntoAtual = document.getElementById('apresentaConjunto').value;
 
-// newValue(result, 5);
-// newValue(result, 15);
+    if(conjuntoAtual == "" || conjuntoAtual == null){
+        msg = "Preencha o Campo!";
+        tipo = "alert-warning";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+        document.getElementById("addvalor").focus();
+    } else {
+        nomeDoArray[conjuntoAtual].clear();
+        exibeValues();
+        msg = "O conjunto <u>" + conjuntoAtual + "</u> foi limpado com sucesso!";
+        tipo = "alert-info";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+        document.getElementById("addvalor").focus();
+    }
+}
 
-// console.log(result);
+function emptySet(){
+    let conjuntoAtual = document.getElementById('apresentaConjunto').value;
+
+    if(conjuntoAtual == "Vazio"){
+        msg = "Não existe Conjunto!";
+        tipo = "alert-warning";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+    } else {
+        nomeDoArray[conjuntoAtual].clear();
+        delete nomeDoArray[conjuntoAtual];
+        exibeConjunto(nomeDoArray);
+        exibeValues();
+        msg = "O conjunto <u>" + conjuntoAtual + "</u> foi apagado com sucesso!";
+        tipo = "alert-info";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+        document.getElementById("conjunto").focus();
+    }    
+}
+
+function sizeSet(){
+    let conjuntoAtual = document.getElementById('apresentaConjunto').value;
+    
+    if(conjuntoAtual == "Vazio"){
+        msg = "Não existe Conjunto!";
+        tipo = "alert-warning";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+    } else {
+        let qnt = nomeDoArray[conjuntoAtual].size();
+        
+        msg = "O conjunto <u>" + conjuntoAtual + "</u> contem <u>" + qnt + "</u> elementos!";
+        tipo = "alert-info";
+        document.getElementById("avisos").innerHTML = notification(msg, tipo);
+        document.getElementById("conjunto").focus();
+    }    
+    
+}
+
+let nomeDoArray = new Object();
